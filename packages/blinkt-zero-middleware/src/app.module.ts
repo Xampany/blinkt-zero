927cf01d0de4b7e262473d7361b0826c81ac9818
Gerd Jungbluth <gerd.jungbluth@engawa.de>
2019-05-04 21:14:46 +0200

    feat(rest): add nest modules

--- packages/blinkt-zero-middleware/src/app.module.ts
+++ packages/blinkt-zero-middleware/src/app.module.ts
@@ -1,9 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { BlinktModule } from "./blinkt/blinkt.module";
+import { BrightnessModule } from "./brightness/brightness.module";
+import { ColorsModule } from "./colors/colors.module";
+import { FlashModule } from "./flash/flash.module";
+import { StripModule } from "./strip/strip.module";
 
 @Module({
-  imports: [],
+  imports: [
+    BlinktModule,
+    ColorsModule,
+    FlashModule,
+    BrightnessModule,
+    StripModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
