c37ace1bee295ec08f115cd4ba0d507a04cbcd32
Gerd Jungbluth <gerd.jungbluth@engawa.de>
2019-05-04 22:26:04 +0200

    feat(colors): add reading of one color

--- packages/blinkt-zero-middleware/src/colors/colors.controller.ts
+++ packages/blinkt-zero-middleware/src/colors/colors.controller.ts
@@ -1,15 +1,32 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Param } from "@nestjs/common";
+import { ApiOperation, ApiUseTags } from "@nestjs/swagger";
 import { Observable, of } from "rxjs";
 import tinycolor from "tinycolor2";
 
 @Controller("colors")
+@ApiUseTags("colors")
 export class ColorsController {
+  private colors = Array.from(Array(8), () => tinycolor.random().toString());
+
   /**
-   * Liefert die Liste der Farben des Blinkt!
+   *
    */
   @Get()
+  @ApiOperation({
+    title: "Liefert die Liste der Farben",
+  })
   readColors(): Observable<string[]> {
-    const colors = Array.from(Array(8), () => tinycolor.random().toString());
-    return of(colors);
+    return of(this.colors);
+  }
+
+  /**
+   *
+   */
+  @Get(":index")
+  @ApiOperation({
+    title: "Liefert die Farbe für den Index",
+  })
+  readColor(@Param("index") index: number): Observable<string> {
+    return of(this.colors[index]);
   }
 }
