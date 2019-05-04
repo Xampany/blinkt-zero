831ec6a79cd12800c562a7daf0797119ea98a212
Gerd Jungbluth <gerd.jungbluth@engawa.de>
2019-05-04 18:35:29 +0200

    feat(rest): add nest

--- index.ts
+++
@@ -1,7 +0,0 @@
-import { Server, ServerResponse, IncomingMessage } from "http";
-
-const server = new Server((req: IncomingMessage, res: ServerResponse) => {
-  res.end(new Date().toISOString());
-});
-
-server.listen(80);
