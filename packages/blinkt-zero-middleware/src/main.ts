49a293118d61048c7bc3c6cfc1bee1ac73cce5ac
Gerd Jungbluth <gerd.jungbluth@engawa.de>
2019-05-04 21:32:59 +0200

    feat(security): add helmet

--- packages/blinkt-zero-middleware/src/main.ts
+++ packages/blinkt-zero-middleware/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from "@nestjs/core";
+import helmet = require("helmet");
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    helmet({
+      noCache: true,
+    }),
+  );
   await app.listen(80);
 }
 bootstrap();
