8712adb56de492ae891a7fff615016027b27f384
Gerd Jungbluth <gerd.jungbluth@engawa.de>
2019-05-04 21:48:22 +0200

    chore(docs): add swagger

--- packages/blinkt-zero-middleware/src/main.ts
+++ packages/blinkt-zero-middleware/src/main.ts
@@ -1,14 +1,27 @@
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import helmet = require("helmet");
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
+  const prefix = "api";
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix(prefix);
   app.use(
     helmet({
       noCache: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle("Blinkt! Raspberry Pi Zero")
+    .setDescription("REST API zur Interaktion zwischen Angular UI und Blinkt!")
+    .setVersion("1.0")
+    .setBasePath(prefix)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
+
   await app.listen(80);
 }
 bootstrap();
