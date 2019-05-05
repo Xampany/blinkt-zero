32e3766c251f10e249d1a665d0a0be4aaea24160
Gerd Jungbluth <gerd.jungbluth@engawa.de>
2019-05-05 18:58:31 +0200

    feat(form): add color form

--- packages/blinkt-zero-ui/src/app/blinkt-dashboard/blinkt-dashboard.module.ts
+++ packages/blinkt-zero-ui/src/app/blinkt-dashboard/blinkt-dashboard.module.ts
@@ -1,14 +1,21 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 import { BlinktCommonModule } from "../blinkt-common/blinkt-common.module";
 import { BlinktDashboardRoutingModule } from "./blinkt-dashboard-routing.module";
+import { ColorFormComponent } from "./color-form/color-form.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { LedListComponent } from "./led-list/led-list.component";
 
 @NgModule({
-  declarations: [DashboardComponent, LedListComponent],
-  imports: [CommonModule, BlinktDashboardRoutingModule, BlinktCommonModule],
+  declarations: [DashboardComponent, LedListComponent, ColorFormComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    BlinktDashboardRoutingModule,
+    BlinktCommonModule
+  ],
   exports: [DashboardComponent]
 })
 export class BlinktDashboardModule {}
---
+++ packages/blinkt-zero-ui/src/app/blinkt-dashboard/color-form/color-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "blinkt-color-form",
+  templateUrl: "./color-form.component.html",
+  styleUrls: ["./color-form.component.scss"]
+})
+export class ColorFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
