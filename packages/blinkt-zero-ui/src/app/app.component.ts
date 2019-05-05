912a6c2071c5a326562e5f8dd198307260c24fce
Gerd Jungbluth <gerd.jungbluth@engawa.de>
2019-05-05 17:38:47 +0200

    feat(ui): add bulma

--- packages/blinkt-zero-ui/src/app/app.component.ts
+++ packages/blinkt-zero-ui/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'blinkt-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "blinkt-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'blinkt-zero-ui';
+  title = "BlinktZero";
 }
