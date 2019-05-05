d55968003d94611438c0b668018c61a259ef9feb
Gerd Jungbluth <gerd.jungbluth@engawa.de>
2019-05-05 11:25:58 +0200

    chore(setup): add cors

--- packages/blinkt-zero-middleware/src/main.ts
+++ packages/blinkt-zero-middleware/src/main.ts
@@ -5,7 +5,9 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const prefix = "api";
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
   app.setGlobalPrefix(prefix);
   app.use(
     helmet({
