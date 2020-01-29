3cdd860335a039b2de25b263193079a8166e3e99
Gerd Jungbluth <gerd.jungbluth@engawa.de>
2020-01-29 19:53:58 +0100

    feat(color): add form to change formatting
    closes #88

--- packages/blinkt-zero-react/src/components/ColorText/ColorText.tsx
+++ packages/blinkt-zero-react/src/components/ColorText/ColorText.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import tinycolor from "tinycolor2";
 
 type Props = {
@@ -20,18 +20,74 @@ type ColorFormat =
   | "hsl"
   | "hsv";
 
+type State = {
+  color: string;
+  format: ColorFormat;
+};
+
+type Action = {
+  type: "select" | "reset";
+  payload?: ColorFormat;
+};
+
 /**
  * Stellt eine Farbe als Text in einem wählbaren Format dar
  * @param param0
  */
 const ColorText: React.FunctionComponent<Props> = ({ color }) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [format, setFormat] = useState<ColorFormat>("name");
-
-  const formattedColor = (c: string, f?: ColorFormat) => {
-    return tinycolor(c).toString(f);
+  const initialState: State = {
+    color,
+    format: "rgb"
   };
-  return <p>{formattedColor(color, format)}</p>;
+
+  const [state, dispatch] = useReducer((state: State, action: Action) => {
+    switch (action.type) {
+      case "select":
+        const format = action.payload as ColorFormat;
+        const next: State = {
+          color: tinycolor(state.color).toString(format),
+          format
+        };
+        return next;
+      case "reset":
+        return {
+          ...initialState
+        };
+      default:
+        throw new Error("Oops");
+    }
+  }, initialState);
+
+  return (
+    <>
+      <form>
+        <select
+          value={state.format}
+          onChange={ev =>
+            dispatch({
+              type: "select",
+              payload: ev.target.value as ColorFormat
+            })
+          }
+        >
+          {["rgb", "hex6", "name"].map(value => (
+            <option key={value} value={value}>
+              {value}
+            </option>
+          ))}
+        </select>
+        <br />
+        <button
+          type="button"
+          disabled={state.format === initialState.format}
+          onClick={() => dispatch({ type: "reset" })}
+        >
+          Reset
+        </button>
+      </form>
+      <p>{state.color}</p>
+    </>
+  );
 };
 
 export default ColorText;
