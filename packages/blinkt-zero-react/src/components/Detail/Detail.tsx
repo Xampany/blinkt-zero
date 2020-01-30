d2384a89bb6c499bde2a819ecf6b4e170bb0e7fd
Gerd Jungbluth <gerd.jungbluth@engawa.de>
2020-01-30 13:01:09 +0100

    feat(routing): add display of color in single led
    closes #92

--- packages/blinkt-zero-react/src/components/Detail/Detail.tsx
+++ packages/blinkt-zero-react/src/components/Detail/Detail.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import Led from "../Led/Led";
+import { useParams } from "react-router-dom";
+import useReadColor from "../../hooks/readColor";
 
 const Detail: React.FC = () => {
+  const { index } = useParams<{ index: string }>();
+
+  const { color, isLoading } = useReadColor(index);
+
   return (
     <>
-      <h1>Detail</h1>
+      {isLoading ? (
+        <p>Bin am Laden am dran am sein...</p>
+      ) : (
+        <Led index={parseInt(index)} color={color}></Led>
+      )}
     </>
   );
 };
